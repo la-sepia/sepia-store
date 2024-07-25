1481863744e7df3a94e1ecbd5c3bb2b12c56c2af
Sergio Gómez Bachiller <sergio.gomez@audiense.com>
2024-07-25 17:48:04 +0200

    feat: update dependencies

--- packages/ingestor/src/main.js
+++ packages/ingestor/src/main.js
@@ -1,6 +1,11 @@
-import { ChromaClient } from "chromadb";
+import { ChromaClient, OpenAIEmbeddingFunction } from "chromadb";
 
 async function main() {
+  const embeddingFunction = new OpenAIEmbeddingFunction({
+    openai_api_key: "sk-proj-YSKiIcXYgVC8H4eaMdWqT3BlbkFJHVnuURnmaYc1yaR5GANO",
+    model: "text-embedding-3-small",
+  });
+
   const client = new ChromaClient({
     path: "http://localhost:8000",
   });
@@ -10,31 +15,15 @@ async function main() {
     metadata: {
       description: "Products of medusa store",
     },
+    embeddingFunction,
   });
-
-  const response = await fetch("http://localhost:9000/store/products");
-
-  if (!response.ok) {
-    console.error("Error conecting");
-    return -1;
-  }
-
-  const { products } = await response.json();
-
-  for (let product of products) {
-    const template = `Product name: ${product.title}. Product description: ${product.description}`;
-    const id = product.handle;
-
-    await collection.upsert({
-      ids: [id],
-      documents: [template],
-    });
-  }
 
   const result = await collection.query({
     queryTexts: "I want something to use in winter",
-    nResults: 1,
+    nResults: 5,
   });
+
+  console.dir(result);
 }
 
 await main();
